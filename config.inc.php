3826fcdaa34ad5f5e69020bd0fd70fd732c9307c
István Gergő <istvangergo96@gmail.com>
2023-05-12 17:16:55 +0200

    Initial commit

---
+++ config.inc.php
@@ -0,0 +1,15 @@
+$fejléc = array(
+'cím' => 'István Gergő',
+);
+
+$oldalak = array(
+'/' => array('fajl' => 'kezdo', 'szoveg' => 'Kezdőlap'),
+'magamrol' => array('fajl' => 'magamrol', 'szoveg' => 'Magamról'),
+'szakmai_oneletrajz' => array('fajl' => 'szakmai_oneletrajz', 
+'szoveg' => 'Szakmai önéletrajz'),
+'galeriak' => array('fajl' => 'galeriak', 'szoveg' => 'Galériák'),
+'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat'),
+);
+
+$hiba_oldal = array ('fajl' => '404', 
+'szoveg' => 'A keresett oldal nem található'); 
